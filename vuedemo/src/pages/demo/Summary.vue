38517d1eec7b42dc918d4bf1f028eae5ba349d04
gonglong <wushan_gong_2006@163.com>
2018-07-22 22:59:21 +0800

    任务清单C

--- vuedemo/src/pages/demo/Summary.vue
+++ vuedemo/src/pages/demo/Summary.vue
@@ -1,6 +1,18 @@
 <template lang="html">
 <div class="comm-page page-summary">
     <h1> demo简介.</h1>
+    <ul class="summ-list">
+        <li>1. 使用elementUI表单 -- form.(TODO: 数据校验)</li>
+        <li>2. 二次封装 elementUI面包屑导航--breadcrumb (TODO: 30%)</li>
+        <li>3. 二次封装 elementUI树形控件--tree (TODO: 数据转换, 踩过坑)</li>
+        <li>4. 使用 elementUI 表格--table (TODO: 数据行多选, 踩过坑)</li>
+        <li>5. 二次封装 elementUI 分页--Pagination (TODO: loading锁)</li>
+    </ul>
+    <ul class="summ-list">
+        <li>1. 熟悉 VUE Router </li>
+        <li>2. 熟悉 VUEX </li>
+        <li>3. vue 常规用法(filter, component)</li>
+    </ul>
 </div>
 </template>
 
@@ -16,6 +28,6 @@ export default {
 
 <style lang="css">
 .page-summary {
-    
+
 }
 </style>
