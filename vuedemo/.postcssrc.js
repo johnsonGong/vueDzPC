e4564744fdec22f3ed70bd85b8a64db7d00fde3f
johnsonGong <wushan_gong_2006@163.com>
2018-08-03 11:38:57 +0800

    增加 postcss插件, px2rem, 暂不应用于 PC端

--- vuedemo/.postcssrc.js
+++ vuedemo/.postcssrc.js
@@ -1,10 +1,13 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
+    'plugins': {
+        'postcss-import': {},
+        'postcss-url': {},
+        // to edit target browsers: use "browserslist" field in package.json
+        'autoprefixer': {}
+        // 手机H5设计稿: width: 750; {remUnit: 75}
+        // PC设计稿: min-width: 1440; {remUnit: 144}; pc端暂不使用 rem;
+        // 'postcss-px2rem': {remUnit: 144}
+    }
 }
